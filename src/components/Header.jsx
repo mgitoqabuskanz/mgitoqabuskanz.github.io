e03223d9c6f8bf42e48fc59e34c37fb49a070dfe
Muhammad Gito Qabus Kanz <mgitoqabuskanz@gmail.com>
2025-02-18 03:49:12 +0700

    Landing Page v1.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 const Header = () => {
   return (
     <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' 
-    style={{backgroundImage: "url('/header_img.jpg')"}} id='Header'>
+    style={{backgroundImage: "linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url('/header_img.jpg')"}} id='Header'>
         <Navbar/>
         <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
             <h1 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>
