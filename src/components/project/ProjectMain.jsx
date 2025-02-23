9a6b44d03c2b128ab5b583e68567acfaae4a3caa
Muhammad Gito Qabus Kanz <mgitoqabuskanz@gmail.com>
2025-02-24 03:36:11 +0700

    Install Slider

--- src/components/project/ProjectMain.jsx
+++ src/components/project/ProjectMain.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { assets } from '../../assets/assets'
+import { assets, mywork_data } from '../../assets/assets'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const ProjectMain = () => {
   return (
@@ -10,96 +13,28 @@ const ProjectMain = () => {
       </h1>
       <p className="mb-10">Passionate About Technologies. Dedicated To Your Needs. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quisquam voluptates, harum aliquid repudiandae quaerat, consequatur atque totam adipisci officiis, deserunt eos. Esse, debitis tenetur possimus corrupti natus beatae quas?</p>
       <div className="flex flex-col md:flex-row w-full gap-6 mb-10">
-        <div className="grid grid-flow-row-dense lg:grid-cols-3 grid-col gap-6">
-          <div className="card bg-base-100 w-fit shadow-sm">
+        <div className="grid lg:grid-cols-3 grid-row gap-6 w-full">
+          {mywork_data.slice(-3).map((work, index) => (
+          <div key={index} className="card bg-base-100 w-fit shadow-sm mx-auto items-center">
             <figure>
               <img
-              src={assets.project1_img}
-              alt="Project 1" />
+              src={work.w_img}
+              alt={work.w_name} />
             </figure>
-            <div className="card-body absolute gap-6 bg-neutral/80 items-center opacity-0 hover:opacity-100 transition-all duration-500">
-              <h2 className="card-title text-accent">Project 1</h2>
-              <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-              <div className="card-actions justify-end">
-                <button className="btn border-accent btn-soft hover:btn-accent">Learn More</button>
+            <div className="card-body w-full h-full absolute gap-6 bg-neutral/80 items-center opacity-0 hover:opacity-100 border hover:border-accent hover:rounded-lg transition-all duration-">
+              <a href={work.w_link}className="card-body w-full h-full absolute" role="button"></a>
+              <h2 className="card-title text-accent">{work.w_name}</h2>
+              <p>
+                {work.w_desc.length > 100 ?
+                  `${work.w_desc.substring(0, 100)} (...)` : work.w_desc
+                }
+              </p>
+              <div className="card-actions justify-end z-10">
+                <a href={work.w_link} className="btn border-accent btn-soft hover:btn-accent">Learn More</a> 
               </div>
             </div>
           </div>
-
-          <div className="card bg-base-100 w-fit shadow-sm">
-            <figure>
-              <img
-              src={assets.project2_img}
-              alt="Project 2" />
-            </figure>
-            <div className="card-body absolute gap-6 bg-neutral/80 items-center opacity-0 hover:opacity-100 transition-all duration-500">
-              <h2 className="card-title text-accent">Project 2</h2>
-              <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-              <div className="card-actions justify-end">
-                <button className="btn border-accent btn-soft hover:btn-accent">Learn More</button>
-              </div>
-            </div>
-          </div>
-
-          <div className="card bg-base-100 w-fit shadow-sm">
-            <figure>
-              <img
-              src={assets.project3_img}
-              alt="Project 3" />
-            </figure>
-            <div className="card-body absolute gap-6 bg-neutral/80 items-center opacity-0 hover:opacity-100 transition-all duration-500">
-              <h2 className="card-title text-accent">Project 3</h2>
-              <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-              <div className="card-actions justify-end">
-                <button className="btn border-accent btn-soft hover:btn-accent">Learn More</button>
-              </div>
-            </div>
-          </div>
-
-          <div className="card bg-base-100 w-fit shadow-sm">
-            <figure>
-              <img
-              src={assets.project4_img}
-              alt="Project 4" />
-            </figure>
-            <div className="card-body absolute gap-6 bg-neutral/80 items-center opacity-0 hover:opacity-100 transition-all duration-500">
-              <h2 className="card-title text-accent">Project 4</h2>
-              <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-              <div className="card-actions justify-end">
-                <button className="btn border-accent btn-soft hover:btn-accent">Learn More</button>
-              </div>
-            </div>
-          </div>
-
-          <div className="card bg-base-100 w-fit shadow-sm">
-            <figure>
-              <img
-              src={assets.project5_img}
-              alt="Project 5" />
-            </figure>
-            <div className="card-body absolute gap-6 bg-neutral/80 items-center opacity-0 hover:opacity-100 transition-all duration-500">
-              <h2 className="card-title text-accent">Project 5</h2>
-              <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-              <div className="card-actions justify-end">
-                <button className="btn border-accent btn-soft hover:btn-accent">Learn More</button>
-              </div>
-            </div>
-          </div>
-
-          <div className="card bg-base-100 w-fit shadow-sm">
-            <figure>
-              <img
-              src={assets.project6_img}
-              alt="Project 6" />
-            </figure>
-            <div className="card-body absolute gap-6 bg-neutral/80 items-center opacity-0 hover:opacity-100 transition-all duration-500">
-              <h2 className="card-title text-accent">Project 6</h2>
-              <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-              <div className="card-actions justify-end">
-                <button className="btn border-accent btn-soft hover:btn-accent">Learn More</button>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
           <button className="btn btn-wide btn-soft hover:btn-accent border-accent">See More</button>
