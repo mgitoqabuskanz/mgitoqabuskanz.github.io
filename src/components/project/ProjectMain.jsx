af336e4daac5ca223087164428bb0399312a841d
mgitoqabuskanz <mgitoqabuskanz@gmail.com>
2025-02-24 19:47:57 +0700

    add project slider

--- src/components/project/ProjectMain.jsx
+++ src/components/project/ProjectMain.jsx
@@ -1,43 +1,59 @@
 import React from 'react'
 import { assets, mywork_data } from '../../assets/assets'
+import ProjectSlider from './ProjectSlider'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import Slider from 'react-slick'
 
 const ProjectMain = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
-    <div className="container mx-auto text-center w-full p-4 md:p-15 md:px-50" id="project">
-      <h1 className="text-3xl md:text-5xl font-extrabold text-accent mb-10"> 
-        <span className="font-extralight text-accent/50">My </span>
-        Project
-      </h1>
-      <p className="mb-10">Passionate About Technologies. Dedicated To Your Needs. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quisquam voluptates, harum aliquid repudiandae quaerat, consequatur atque totam adipisci officiis, deserunt eos. Esse, debitis tenetur possimus corrupti natus beatae quas?</p>
-      <div className="flex flex-col md:flex-row w-full gap-6 mb-10">
-        <div className="grid lg:grid-cols-3 grid-row gap-6 w-full">
-          {mywork_data.slice(-3).map((work, index) => (
-          <div key={index} className="card bg-base-100 w-fit shadow-sm mx-auto items-center">
-            <figure>
-              <img
-              src={work.w_img}
-              alt={work.w_name} />
-            </figure>
-            <div className="card-body w-full h-full absolute gap-6 bg-neutral/80 items-center opacity-0 hover:opacity-100 border hover:border-accent hover:rounded-lg transition-all duration-">
-              <a href={work.w_link}className="card-body w-full h-full absolute" role="button"></a>
-              <h2 className="card-title text-accent">{work.w_name}</h2>
-              <p>
-                {work.w_desc.length > 100 ?
-                  `${work.w_desc.substring(0, 100)} (...)` : work.w_desc
-                }
-              </p>
-              <div className="card-actions justify-end z-10">
-                <a href={work.w_link} className="btn border-accent btn-soft hover:btn-accent">Learn More</a> 
-              </div>
-            </div>
-          </div>
-          ))}
-        </div>
+    <div className="container mx-auto text-center w-full p-15" id="project">
+      <div className="block lg:w-3/4 w-full mx-auto">
+        <h1 className="text-3xl md:text-5xl font-extrabold text-accent mb-10"> 
+          <span className="font-extralight text-accent/50">My </span>
+          Project
+        </h1>
+        <p className="mb-10">Passionate About Technologies. Dedicated To Your Needs. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quisquam voluptates, harum aliquid repudiandae quaerat, consequatur atque totam adipisci officiis, deserunt eos. Esse, debitis tenetur possimus corrupti natus beatae quas?</p>
       </div>
-          <button className="btn btn-wide btn-soft hover:btn-accent border-accent">See More</button>
+      
+      <div className="block w-full lg:px-15 px-2 mb-10">
+        <ProjectSlider/>
+      </div>
+      <button className="btn btn-wide btn-soft hover:btn-accent border-accent">See More</button>
     </div>
   )
 }
---
+++ src/components/project/ProjectSlider.jsx
@@ -0,0 +1,83 @@
+import React, { Fragment } from 'react'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { assets, mywork_data } from '../../assets/assets'
+
+const ProjectSlider = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <Slider {...settings}>
+      {mywork_data.slice(0,6).map((work, index) => (
+
+      <div key={index} class="flex items-center justify-center px-4">
+        <a
+        class="relative block bg-neutral/50 group rounded-lg"
+        href="##"
+        >
+          <img
+          class="absolute inset-0 object-cover w-full h-full group-hover:opacity-30 transition-all duration-500 rounded-lg"
+          src={work.w_img} alt={work.w_name}
+          />
+          <div class="relative p-6">
+            <div class="">
+              <div
+              class="transition-all duration-500 transform opacity-0 group-hover:opacity-100 rounded-lg"
+              >
+                <div class="space-y-6 p-2">
+                  <h2 className="text-2xl text-accent">{work.w_name}</h2>
+                  <p className="text-sm">
+                    {work.w_desc.length > 100 ?
+                      `${work.w_desc.substring(0, 100)} (...)` : work.w_desc
+                    }
+                  </p>
+                  <a
+                  href={work.w_link} class="px-4 py-2 text-sm btn btn-soft btn-accent"
+                  >
+                    Learn More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+      ))}
+    </Slider>
+  )
+}
+
+export default ProjectSlider
