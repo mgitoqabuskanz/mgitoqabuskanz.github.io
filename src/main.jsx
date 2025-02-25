22641a0be14526c23bfa3c7910fa44b38a3228c1
mgitoqabuskanz <mgitoqabuskanz@gmail.com>
2025-02-25 10:06:43 +0700

    test page

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/<mgitoqabuskanz.github.io>/"}>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
