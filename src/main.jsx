0c38e35b1e743315e64d61bf4f226ca5016f6ca1
mgitoqabuskanz <mgitoqabuskanz@gmail.com>
2025-02-25 13:12:18 +0700

    test page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+ReactDOM.render(
+  <StrictMode>
       <App />
-  </React.StrictMode>
+  </StrictMode>,
+  document.getElementById('root'),
 );
